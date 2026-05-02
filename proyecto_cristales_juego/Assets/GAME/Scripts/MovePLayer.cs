c493346991d7b2fe09a146ded8d757d91fa9e353
mxom671 <maria_val.ospina@uao.edu.co>
2026-05-02 10:32:06 -0500

    Movimiento player
    Se le coloca el rigibody y el box collider al player y se hacen pruebas

--- proyecto_cristales_juego/Assets/GAME/Scripts/MovePLayer.cs
+++ proyecto_cristales_juego/Assets/GAME/Scripts/MovePLayer.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 
-public class MovePLayer : MonoBehaviour
+public class MovePlayer : MonoBehaviour
 {
+    public float speed = 5f; //speed of player's movement
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
 }
