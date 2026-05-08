27c90ab1552f0169699bc8d9d48f1c0a67125ac5
mxom671 <maria_val.ospina@uao.edu.co>
2026-05-08 07:55:58 -0500

    Actualizacion
    Cambio de fondos, iluminacion, agregar "cristales" (capsulas) para pruebas, agregar scripts de movimiento y control y hacer pruebas

--- proyecto_cristales_juego/Assets/GAME/Scripts/MovePLayer.cs
+++ proyecto_cristales_juego/Assets/GAME/Scripts/MovePLayer.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public float speed = 5f; //speed of player's movement
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    public Transform camara;
+    private CharacterController controller;
+
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        // Usamos el sistema de siempre
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camara.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
     }
 }
